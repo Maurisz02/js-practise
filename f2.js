0f7bd75cf16476fb1717c024d6bd09680ff31df3
Maurisz <p2002adam@gmail.com>
2022-09-08 12:32:05 +0200

    javitas

--- f2.js
+++ f2.js
@@ -1,7 +1,7 @@
 var tomb = [];
 var eves = [];
 var num;
-for(var i = 0; i < 20;i++){
+for(var i = 0; i < 20; i++){
     var random = Math.floor(Math.random() * 21)+2;
     var cimsor= i +1;
     tomb[i]={'nev': 'Cirmos#'+cimsor,'eletkor': random};
