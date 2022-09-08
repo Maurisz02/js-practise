af6f8a39a9ad3309f6eeb72b3804eb8fb8d1ba1c
Maurisz <p2002adam@gmail.com>
2022-09-08 12:34:18 +0200

    name javitas

--- f2.js
+++ f2.js
@@ -4,7 +4,7 @@ var num;
 for(var i = 0; i < 20; i++){
     var random = Math.floor(Math.random() * 21)+2;
     var cimsor= i +1;
-    tomb[i]={'nev': 'Cirmos#'+cimsor,'eletkor': random};
+    tomb[i]={'nev': 'Cirmos'+cimsor,'eletkor': random};
     if(random == 2){
         eves.push(tomb[i]);
     }
