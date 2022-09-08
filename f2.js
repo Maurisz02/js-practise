9400069a6583557ef77cb32ce48fa66c3125c1ae
Maurisz <p2002adam@gmail.com>
2022-09-08 12:14:37 +0200

    Task 2 done

---
+++ f2.js
@@ -0,0 +1,13 @@
+var tomb = [];
+var eves = [];
+var num;
+for(var i = 0; i < 20;i++){
+    var random = Math.floor(Math.random() * 21)+2;
+    var cimsor= i +1;
+    tomb[i]={'nev': 'Cirmos#'+cimsor,'eletkor': random};
+    if(random == 2){
+        eves.push(tomb[i]);
+    }
+}
+console.log(tomb);
+console.log(eves);
